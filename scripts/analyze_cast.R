4524d49a39f63db3e63f2f7c5ab3d60d1cbaf738
= <conorotompkins@gmail.com>
2020-06-05 19:11:16 +0000

    calculated avg number of lines per cast_name

--- scripts/analyze_cast.R
+++ scripts/analyze_cast.R
@@ -44,10 +44,10 @@ df_cast_long <- df_cast %>%
   mutate(season_cast_count = sum(cast_flag == TRUE),
          season_line_count = sum(line_count, na.rm = TRUE),
          lines_per_episode = season_line_count / season_cast_count) %>% 
-  ungroup() %>% 
-  mutate(cast_name = reorder_within(x = cast_name, by = season_line_count, within = season))
+  ungroup()
 
 df_cast_long %>% 
+  mutate(cast_name = reorder_within(x = cast_name, by = season_line_count, within = season)) %>% 
   filter(season_cast_count > 1) %>% 
   filter(season == 5) %>% 
   ggplot(aes(episode, cast_name, fill = line_count)) +
@@ -57,3 +57,20 @@ df_cast_long %>%
     scale_y_reordered(expand = c(0,0)) +
     facet_wrap(~season, scales = "free") +
     theme(panel.grid = element_blank())
+
+df_cast_count <- df_cast_long %>% 
+  filter(cast_flag == TRUE) %>% 
+  count(cast_name, sort = TRUE) %>% 
+  filter(n >= 10)
+
+df_cast_long %>% 
+  semi_join(df_cast_count) %>% 
+  filter(cast_flag == TRUE) %>% 
+  group_by(season, episode, cast_name) %>% 
+  summarize(line_count = sum(line_count)) %>% 
+  group_by(cast_name) %>% 
+  summarize(line_avg = mean(line_count)) %>% 
+  arrange(desc(line_avg)) %>% 
+  View()
+
+            
